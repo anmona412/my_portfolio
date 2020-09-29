72f213a32eac0961730736eadda571a2f557f66c
anmona412 <anmo.knight412@gmail.com>
2020-09-29 18:28:32 +0900

    更新履歴モデルとコントローラ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_29_072544) do
+ActiveRecord::Schema.define(version: 2020_09_29_092529) do
+
+  create_table "updates", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "update", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false

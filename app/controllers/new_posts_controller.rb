b5d10cfc1f03536aa27342955db1610c88825f51
anmona412 <anmo.knight412@gmail.com>
2020-09-29 19:58:15 +0900

    更新履歴機能追加

--- app/controllers/new_posts_controller.rb
+++ app/controllers/new_posts_controller.rb
@@ -4,12 +4,14 @@ class NewPostsController < ApplicationController
   end
 
   def create
-    @post = NewPost.create(new_post: params[:new_post])
+    @post = NewPost.create(new_post_params)
     if @post.save
       redirect_to root_path
     end
   end
 
   private
-
+  def new_post_params
+    params.require(:new_post).permit(:new_post)
+  end
 end
